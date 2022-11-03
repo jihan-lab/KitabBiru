d2cee90be22cccde184e6132101c0326bd74e345
Jihan Abdul Rohman <jihannabdul@gmail.com>
2022-11-03 15:10:16 +0700

    First Commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import {StyleSheet, Text, View} from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text>Hello World</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default App;
